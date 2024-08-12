c2b5b50469e111bea47e31cacc85bfe2cff6de7c
welle-ai <fall.momar_aly_dom@uqam.ca>
2024-08-12 12:43:40 -0400

    Update 1_env.sh

--- 1_env.sh
+++ 1_env.sh
@@ -2,4 +2,4 @@ export PROJECT=$(gcloud config get-value project)
 export REGION=us
 export NAME=docai-service
 export GCP_DOCAI_REGION=us
-export GCP_DOCAI_PROCESSOR_ID=876dad7e2e8ff191
+export GCP_DOCAI_PROCESSOR_ID=bb0c52847ff74363
